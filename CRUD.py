2f399ed24efdac1f08d25c4892afe2c63f5b1a3d
brenda njoki <ngunjiribrenda5@gmail.com>
2025-08-28 15:48:12 +0300

    Implement CRUD functions for managing accessories and add main script file

--- CRUD.py
+++ CRUD.py
@@ -1,2 +1,39 @@
 from models import session, Accessories, Customers, Sales, Base, engine
 
+
+#Add a new accessory
+def add_accessory(name, accessory_type, price, stock):
+    new_accessory = Accessories(name=name, accessory_type=accessory_type, price=price, stock=stock)
+    session.add(new_accessory)
+    session.commit()
+    print(f"Added accessory: {name}")
+
+
+#Update accessrory stock
+def update_accessory_stock(accessory_id, new_stock):
+    accessory = session.query(Accessories.filter_by(id=accessory_id).first())
+    if accessory:
+        accessory.stock = new_stock
+        session.commit()
+
+#View all accessories
+def view_accessories():
+    accessories = session.query(Accessories).all()
+    for accessory in accessories:
+        print(f"Id: {accessory.id}, Name: {accessory.name}, Accessory_type: {accessory.accessory_type}, Price: {accessory.price}, Stock: {accessory.stock}")
+        return accessories
+    
+
+#Delete accessory
+def delete_accessory(accssory_id):
+    accessories = session.query(Accessories.filter_by(id=accessories).first())
+
+    if accessories:
+        session.delete(accessories)
+        session.commit()
+        
+    
+
+                              
+
+
---
+++ main.py
@@ -0,0 +1,8 @@
+#mport click_
+
+#rom CRUD import add_accessory, add_customer, add_sales
+
+
+    
+    
+
