1cc69964cee34df4903166ae9aed0f9e2eb3a48f
brenda njoki <ngunjiribrenda5@gmail.com>
2025-08-29 09:46:30 +0300

    Creates add CRUD operations

--- CRUD.py
+++ CRUD.py
@@ -9,29 +9,6 @@ def add_accessory(name, accessory_type, price, stock):
     print(f"Added accessory: {name}")
 
 
-#Update accessrory stock
-def update_accessory_stock(accessory_id, new_stock):
-    accessory = session.query(Accessories.filter_by(id=accessory_id).first())
-    if accessory:
-        accessory.stock = new_stock
-        session.commit()
-
-#View all accessories
-def view_accessories():
-    accessories = session.query(Accessories).all()
-    for accessory in accessories:
-        print(f"Id: {accessory.id}, Name: {accessory.name}, Accessory_type: {accessory.accessory_type}, Price: {accessory.price}, Stock: {accessory.stock}")
-        return accessories
-    
-
-#Delete accessory
-def delete_accessory(accssory_id):
-    accessories = session.query(Accessories.filter_by(id=accessories).first())
-
-    if accessories:
-        session.delete(accessories)
-        session.commit()
-        
     
 
                               
