bbb52f09fa128d29867c38162fb239de04945772
brenda njoki <ngunjiribrenda5@gmail.com>
2025-08-29 10:11:48 +0300

    Creates View CRUD operations

--- CRUD.py
+++ CRUD.py
@@ -19,4 +19,10 @@ def update_accessory_stock(accessory_id, new_stock):
         print(f"Accessory with ID {accessory_id}not found")
 
 
+#view all accessories
+def view_accessories():
+    accessories = session.querry(Accessories).all()
+    for accessory in accessories:
+        print(f'ID: {accessory.id}, Name: {accessory.name}, Type:{accessory.accessory_type}, Price: ${accessory.price}, Stock:{accessory.stock}')
+        return accessories
     
